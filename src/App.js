b9f41527622f37870f4cc45bb6eb5ba6a83b3d45
pmarxbraun <p.marinbraun@gmail.com>
2019-10-03 20:05:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,72 +8,72 @@ import {
     Form,
     Dimmer,
     Loader
-  } from "semantic-ui-react";
-  import DatetimePicker from 'react-semantic-datetime';
-  import moment from 'moment';
-  //import debounce from 'debounce';
+} from "semantic-ui-react";
+import DatetimePicker from 'react-semantic-datetime';
+import moment from 'moment';
+//import debounce from 'debounce';
 
 
 
 export default class App extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
 
         this.state = {
-            wastePoints : {
-                infos : [
+            wastePoints: {
+                infos: [
                     {
-                    coords : [48.975307, 1.752872],
-                    name : 'VEOLIA',
-                    polygon: [],
-                    color : 'blue'
+                        coords: [49.001965, 1.7036777],
+                        name: 'Allée des Marronniers 78200 Mantes la Jolie',
+                        polygon: [],
+                        color: 'blue'
                     },
                     {
-                    coords : [48.384035, 2.963927],
-                    name : 'SMAB',
-                    polygon: [],
-                    color : 'orange'
+                        coords: [48.9787739, 1.7289454],
+                        name: 'Rue de la Vaucouleurs 78711 Mantes la Ville',
+                        polygon: [],
+                        color: 'orange'
                     },
                     {
-                    coords : [49.311205, 1.014449],
-                    name : 'Sonolub',
-                    polygon: [],
-                    color : 'violet'
+                        coords: [48.9892923, 1.6916927],
+                        name: 'Avenue de la grande Halle 78200 Buchelay',
+                        polygon: [],
+                        color: 'violet'
                     },
                     {
-                    coords : [48.948978, 2.299089],
-                    name : 'Sevia',
-                    polygon: [],
-                    color : 'green'
+                        coords: [48.975589, 1.8532391],
+                        name: 'Boulevard Pierre Lefaucheux 78410 Aubergenvillevia',
+                        polygon: [],
+                        color: 'green'
                     }
                 ],
             },
             options: {
-                zoom: 7,
+                zoom: 9,
                 type: 'time',
                 range: 7200,
-                mode: 'truck',
+                mode: 'car',
                 traffic: 'enabled',
                 style: 'reduced.day',
                 time: new Date().toISOString()
             },
-            clientsCoords : [],
-            selectedAdressIndex : 0,
-            displayCalendar:false,
-            isolineInfos:[],
-            isoFirstReduce:[],
-            isoSecReduce:[],
-            isoThirdReduce:[],
+            clientsCoords: [],
+            selectedAdressIndex: 0,
+            displayCalendar: false,
+            isolineInfos: [],
+            isoFirstReduce: [],
+            isoSecReduce: [],
+            isoThirdReduce: [],
             loading: true,
         }
 
         this.mapBlock = React.createRef()
-    
+
     }
 
     handleSelectAdress = (value) => {
-        this.setState({selectedAdressIndex : value}, ()=>{ this.updateIsolines() })
+        this.setState({ selectedAdressIndex: value }, () => { this.updateIsolines() })
     }
 
     handleFormChange = (event) => {
@@ -81,21 +81,21 @@ export default class App extends Component {
         const value = event.target.value;
         const copy = this.state.options;
         if (option === 'type' && this.state.options.range > maxIsolineRangeLookup[value]) {
-           copy.range = maxIsolineRangeLookup[value];
+            copy.range = maxIsolineRangeLookup[value];
         }
         copy[option] = value;
-        
+
         this.setState({
-           options: copy
+            options: copy
         }/*, () => {
            this.updateIsolines();
         }*/);
-     
-     }
+
+    }
 
     updateIsolines = () => {
 
-        this.setState({loading : true })
+        this.setState({ loading: true })
 
         let isolines = [];
         let isoFirst = [];
@@ -103,120 +103,120 @@ export default class App extends Component {
         let isoThird = [];
 
         let isoReduceFirstOpt = {
-                                    zoom: this.state.options.zoom,
-                                    type: 'time',
-                                    range: 5400,
-                                    mode: 'truck',
-                                    traffic: 'enabled',
-                                    style: this.state.options.style,
-                                    time: this.state.options.time
-                                }
-        let isoSecOpt =     {
-                                zoom: this.state.options.zoom,
-                                type: 'time',
-                                range: 3600,
-                                mode: 'truck',
-                                traffic: 'enabled',
-                                style: this.state.options.style,
-                                time: this.state.options.time
-                            }
-        let isoThirdOpt =     {
-                                zoom: this.state.options.zoom,
-                                type: 'time',
-                                range: 1800,
-                                mode: 'truck',
-                                traffic: 'enabled',
-                                style: this.state.options.style,
-                                time: this.state.options.time
-                            }
+            zoom: this.state.options.zoom,
+            type: 'time',
+            range: 5400,
+            mode: 'car',
+            traffic: 'enabled',
+            style: this.state.options.style,
+            time: this.state.options.time
+        }
+        let isoSecOpt = {
+            zoom: this.state.options.zoom,
+            type: 'time',
+            range: 3600,
+            mode: 'car',
+            traffic: 'enabled',
+            style: this.state.options.style,
+            time: this.state.options.time
+        }
+        let isoThirdOpt = {
+            zoom: this.state.options.zoom,
+            type: 'time',
+            range: 1800,
+            mode: 'car',
+            traffic: 'enabled',
+            style: this.state.options.style,
+            time: this.state.options.time
+        }
 
-        const promise =  fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
-                                              this.state.options))
-                            .then(x => x.json());
-        
-        const isoReduceOne =  fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
-                                              isoReduceFirstOpt))
-                            .then(x => x.json());
+        const promise = fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
+            this.state.options))
+            .then(x => x.json());
 
-        const isoReduceSec =  fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
+        const isoReduceOne = fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
+            isoReduceFirstOpt))
+            .then(x => x.json());
+
+        const isoReduceSec = fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
             isoSecOpt))
-              .then(x => x.json());
+            .then(x => x.json());
 
-        const isoReduceThird =  fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
+        const isoReduceThird = fetch(hereIsolineUrl(this.state.wastePoints.infos[this.state.selectedAdressIndex].coords,
             isoThirdOpt))
-                .then(x => x.json());
-                            
+            .then(x => x.json());
+
         Promise.resolve(promise).then(res => {
-           isolines = res.response.isoline[0].component[0].shape;
-           isolines = isolines.map(x => [x.split(',')[0], x.split(',')[1]])
-           this.setState({ isolineInfos : isolines})
+            isolines = res.response.isoline[0].component[0].shape;
+            isolines = isolines.map(x => [x.split(',')[0], x.split(',')[1]])
+            this.setState({ isolineInfos: isolines })
         });
 
         Promise.resolve(isoReduceOne).then(res => {
             isoFirst = res.response.isoline[0].component[0].shape;
             isoFirst = isoFirst.map(x => [x.split(',')[0], x.split(',')[1]])
-            this.setState({ isoFirstReduce : isoFirst})
-         });
+            this.setState({ isoFirstReduce: isoFirst })
+        });
 
-         Promise.resolve(isoReduceSec).then(res => {
+        Promise.resolve(isoReduceSec).then(res => {
             isoSec = res.response.isoline[0].component[0].shape;
             isoSec = isoSec.map(x => [x.split(',')[0], x.split(',')[1]])
-            this.setState({ isoSecReduce : isoSec})
-         });
+            this.setState({ isoSecReduce: isoSec })
+        });
 
-         Promise.resolve(isoReduceThird).then(res => {
+        Promise.resolve(isoReduceThird).then(res => {
             isoThird = res.response.isoline[0].component[0].shape;
             isoThird = isoThird.map(x => [x.split(',')[0], x.split(',')[1]])
-            this.setState({ isoThirdReduce : isoThird, loading : false})
-         });
+            this.setState({ isoThirdReduce: isoThird, loading: false })
+        });
 
-     }
+    }
 
-     convertSecToHours = (sec) => {
-         let hours = sec / 3600
-         hours = Math.floor(hours)
+    convertSecToHours = (sec) => {
+        let hours = sec / 3600
+        hours = Math.floor(hours)
 
-         let minutes = (sec - (3600 * hours))/60
-         minutes = Math.floor(minutes)
+        let minutes = (sec - (3600 * hours)) / 60
+        minutes = Math.floor(minutes)
 
         return (`${hours} h ${minutes} mn`)
-     }
-     
-     componentDidMount = () => {
+    }
+
+    componentDidMount = () => {
         this.updateIsolines();
-     }
+    }
 
 
     render() {
 
         const max = this.state.options.type === 'distance' ?
-        maxIsolineRangeLookup.distance :
-        maxIsolineRangeLookup.time;
+            maxIsolineRangeLookup.distance :
+            maxIsolineRangeLookup.time;
 
         const sliderVal = this.state.options.range > max ? max : this.state.options.range;
 
         return (
-                <div>
-                    <h2>Carte isochrone camions</h2>
+            <div>
+                <h2>Carte isochrone camions</h2>
 
 
-                    <div className="selectAdress">
-                        <Label size='large' color='teal'>Choix du site</Label>
-                            {
-                                this.state.wastePoints.infos.map((value, index) => {
-                                    return <Button
-                                                color={ this.state.selectedAdressIndex === index && this.state.wastePoints.infos[index].color}
-                                                key={index}
-                                                onClick={() => this.handleSelectAdress(index)}
-                                            >
-                                            {value.name}
-                                            </Button>
+                <div className="selectAdress">
+                    <Label size='large' color='teal'>Choix du site</Label>
+                    {
+                        this.state.wastePoints.infos.map((value, index) => {
+                            return <Button
+                                color={this.state.selectedAdressIndex === index && this.state.wastePoints.infos[index].color}
+                                key={index}
+                                onClick={() => this.handleSelectAdress(index)}
+                            >
+                                {value.name}
+                            </Button>
 
-                                })
-                            }
-                    </div>
+                        })
+                    }
+                </div>
 
-                    <div className="controls">
+                <div className="controls">
 
                     {/*<div>
                     <label htmlFor="type">Distance / Durée</label>
@@ -264,10 +264,10 @@ export default class App extends Component {
                         />
                     </div>*/}
                     <div >
-                        <div style={{display:'flex'}}><div style={{width:'8px', height:'8px', backgroundColor:'#cf0808', marginRight:'3px', marginTop:'3px'}}></div><p style={{fontSize:'10px'}}>0 à 30 mn</p></div>
-                        <div style={{display:'flex'}}><div style={{width:'8px', height:'8px', backgroundColor:'#e89923', marginRight:'3px', marginTop:'3px'}}></div><p style={{fontSize:'10px'}}>30 mn à 1 h</p></div>
-                        <div style={{display:'flex'}}><div style={{width:'8px', height:'8px', backgroundColor:'#d3d938', marginRight:'3px', marginTop:'3px'}}></div><p style={{fontSize:'10px'}}>1 h à 1 h 30</p></div>
-                        <div style={{display:'flex'}}><div style={{width:'8px', height:'8px', backgroundColor:'#31a833', marginRight:'3px', marginTop:'3px'}}></div><p style={{fontSize:'10px'}}>1 h 30 à 2 h</p></div>
+                        <div style={{ display: 'flex' }}><div style={{ width: '8px', height: '8px', backgroundColor: '#cf0808', marginRight: '3px', marginTop: '3px' }}></div><p style={{ fontSize: '10px' }}>0 à 30 mn</p></div>
+                        <div style={{ display: 'flex' }}><div style={{ width: '8px', height: '8px', backgroundColor: '#e89923', marginRight: '3px', marginTop: '3px' }}></div><p style={{ fontSize: '10px' }}>30 mn à 1 h</p></div>
+                        <div style={{ display: 'flex' }}><div style={{ width: '8px', height: '8px', backgroundColor: '#d3d938', marginRight: '3px', marginTop: '3px' }}></div><p style={{ fontSize: '10px' }}>1 h à 1 h 30</p></div>
+                        <div style={{ display: 'flex' }}><div style={{ width: '8px', height: '8px', backgroundColor: '#31a833', marginRight: '3px', marginTop: '3px' }}></div><p style={{ fontSize: '10px' }}>1 h 30 à 2 h</p></div>
                     </div>
                     <div>
                         <label htmlFor="style">Map Style</label>
@@ -285,7 +285,7 @@ export default class App extends Component {
 
                     {<div>
                         <Button
-                            onClick={() => {this.setState({displayCalendar :! this.state.displayCalendar})}}
+                            onClick={() => { this.setState({ displayCalendar: !this.state.displayCalendar }) }}
                         >Selectionner une date</Button>
 
 
@@ -293,17 +293,17 @@ export default class App extends Component {
                             this.state.displayCalendar &&
                             <div>
                                 <Form.Input
-                                action={{ color: 'teal', icon: 'calendar' }}
-                                actionPosition="left"
-                                value={moment(this.state.options.time).format('MMMM Do YYYY, h:mm:ss a')}
-                                fluid
+                                    action={{ color: 'teal', icon: 'calendar' }}
+                                    actionPosition="left"
+                                    value={moment(this.state.options.time).format('MMMM Do YYYY, h:mm:ss a')}
+                                    fluid
                                 />
                                 <DatetimePicker
 
                                     onChange={value => {
                                         let copy = this.state.options;
                                         copy.time = value.toISOString()
-                                        this.setState({ options : copy})
+                                        this.setState({ options: copy })
                                     }} // Mandatory
                                     value={moment(this.state.options.time).format('MMMM Do YYYY, h:mm:ss a')} // Mandatory
                                     time={true} // optional to show time selection, just a date picket if false (default:true)
@@ -312,19 +312,19 @@ export default class App extends Component {
                         }
                     </div>}
 
-                        <div >
-                            <Button color='linkedin'
-                                    onClick={()=> {
-                                                    this.setState({loading : true})
-                                                    this.updateIsolines()
-                                                }
-                                            }>Envoyer ces valeurs</Button>
-                        </div>
-                        
+                    <div >
+                        <Button color='linkedin'
+                            onClick={() => {
+                                this.setState({ loading: true })
+                                this.updateIsolines()
+                            }
+                            }>Envoyer ces valeurs</Button>
                     </div>
-        
-                    {
-                        this.state.loading 
+
+                </div>
+
+                {
+                    this.state.loading
                         ?
 
                         <Dimmer active inverted>
@@ -332,28 +332,28 @@ export default class App extends Component {
                         </Dimmer>
 
                         :
-                    <div                         
-                    ref={this.mapBlock}>
+                        <div
+                            ref={this.mapBlock}>
 
-                        <MapContainer
-                            center={this.state.wastePoints.infos[this.state.selectedAdressIndex].coords}
-                            options={this.state.options}
-                            polygon={this.state.isolineInfos}
-                            isoFirstReduce={this.state.isoFirstReduce}
-                            isoSecReduce = {this.state.isoSecReduce}
-                            isoThirdReduce = {this.state.isoThirdReduce}
-                            style={this.state.options.style}
-                            markers={this.state.wastePoints.infos}
-                            indexSelected={this.state.selectedAdressIndex}
-                        />
-                    
-                    </div>
-                    }
-                   
-                    
-                
-                </div>                  
-                    
+                            <MapContainer
+                                center={this.state.wastePoints.infos[this.state.selectedAdressIndex].coords}
+                                options={this.state.options}
+                                polygon={this.state.isolineInfos}
+                                isoFirstReduce={this.state.isoFirstReduce}
+                                isoSecReduce={this.state.isoSecReduce}
+                                isoThirdReduce={this.state.isoThirdReduce}
+                                style={this.state.options.style}
+                                markers={this.state.wastePoints.infos}
+                                indexSelected={this.state.selectedAdressIndex}
+                            />
+
+                        </div>
+                }
+
+
+
+            </div>
+
 
         );
     }
--- src/components/CustomMarker.js
+++ src/components/CustomMarker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Marker, Popup} from 'react-leaflet';
+import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet'
 import { hereCredentials } from "../modules/here";
 
@@ -13,70 +13,71 @@ var iconUrl = 'data:image/svg+xml;base64,' + btoa(svg);
 
 
 var greenIcon = new L.Icon({
-    iconUrl: iconUrl,
-    iconSize: [6, 10],
-    iconAnchor: [3, 10],
-    popupAnchor: [1, -34],
-  });
+  iconUrl: iconUrl,
+  iconSize: [6, 10],
+  iconAnchor: [3, 10],
+  popupAnchor: [1, -34],
+});
 
 
 class CustomMarker extends React.Component {
 
-  state={
-    isSearch:false,
-    infos : null
+  state = {
+    isSearch: false,
+    infos: null
   }
 
   convertSecToHours = (sec) => {
     let hours = sec / 3600
     hours = Math.floor(hours)
 
-    let minutes = (sec - (3600 * hours))/60
+    let minutes = (sec - (3600 * hours)) / 60
     minutes = Math.floor(minutes)
 
-   return (`${hours} h ${minutes} mn`)
-}
+    return (`${hours} h ${minutes} mn`)
+  }
 
   routing = () => {
     var infos = {}
 
-    const promise = fetch(`https://route.api.here.com/routing/7.2/calculateroute.json?waypoint0=${this.props.position}&waypoint1=${this.props.wastePoint}&mode=fastest%3Btruck&app_id=${hereCredentials.id}&app_code=${hereCredentials.code}`)
-    .then(x =>  x.json())
+    const promise = fetch(`https://route.api.here.com/routing/7.2/calculateroute.json?waypoint0=${this.props.position}&waypoint1=${this.props.wastePoint}&mode=fastest%3Bcar&app_id=${hereCredentials.id}&app_code=${hereCredentials.code}`)
+      .then(x => x.json())
 
-    Promise.resolve(promise).then(res => {infos = {
-      time : res.response.route[0].summary.trafficTime,
-      dist : res.response.route[0].summary.distance,
+    Promise.resolve(promise).then(res => {
+      infos = {
+        time: res.response.route[0].summary.trafficTime,
+        dist: res.response.route[0].summary.distance,
       }
 
       console.log(res)
 
-      this.setState({infos : infos})
+      this.setState({ infos: infos })
     })
 
 
   }
 
-  render(){
-    return  (
+  render() {
+    return (
       <Marker position={this.props.position}
-              icon={greenIcon}
-        >
-          <Popup>
+        icon={greenIcon}
+      >
+        <Popup>
           <p>{this.props.client}</p>
           <p>{this.props.adress}</p>
           <button
-          onClick={()=> this.routing() }>Calcul</button>
+            onClick={() => this.routing()}>Calcul</button>
           {
-            this.state.infos && 
+            this.state.infos &&
             <div>
               <p>Distance : {this.state.infos.dist / 1000} km</p>
               <p>Temps : {this.convertSecToHours(this.state.infos.time)}</p>
-              <p>Emission CO2 : {+((this.state.infos.dist / 1000)*1.09).toFixed(2)} KG</p>
+              <p>Emission CO2 : {+((this.state.infos.dist / 1000) * 1.09).toFixed(2)} KG</p>
             </div>
           }
-          </Popup>
+        </Popup>
       </Marker>)
-          }
+  }
 }
 
 export default CustomMarker;
